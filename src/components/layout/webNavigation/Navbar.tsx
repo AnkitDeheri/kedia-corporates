95efcc945af573e8619a012479ae4518e2298846
Ankit <ankitdeheri2610@gmail.com>
2025-08-28 23:54:20 +0530

    changes

--- src/components/layout/webNavigation/Navbar.tsx
+++ src/components/layout/webNavigation/Navbar.tsx
@@ -150,7 +150,7 @@ const Navbar = () => {
                 },
               }}
             >
-              KEDIA CORPORATES123
+              KEDIA CORPORATES
             </Typography>
           </Link>
 
