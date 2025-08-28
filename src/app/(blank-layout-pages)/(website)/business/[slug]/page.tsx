449e63cbeb57b1b5ad75cc2413a51ec844c0d9cb
Ankit <ankitdeheri2610@gmail.com>
2025-08-28 14:43:33 +0530

    slug page changes

--- src/app/(blank-layout-pages)/(website)/business/[slug]/page.tsx
+++ src/app/(blank-layout-pages)/(website)/business/[slug]/page.tsx
@@ -1,23 +1,14 @@
 import BusinessClient from "@/components/business/BusinessClient";
 import React from "react";
-import { useRouter } from "next/router";
 
-const BusinessClientPage = ({ slug }: { slug: string }) => {
+const BusinessClientPage = ({ params }: { params: { slug: string } }) => {
+  const { slug } = params;
+
   return (
     <div>
       <BusinessClient slug={slug} />
     </div>
   );
 };
-
-export async function getServerSideProps(context: { params: { slug: string } }) {
-  const { slug } = context.params;
-
-  return {
-    props: {
-      slug,
-    },
-  };
-}
 
 export default BusinessClientPage;
